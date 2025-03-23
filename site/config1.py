2ec4d3c5379dfbadbb13ce3cbe79251e88c44bd2
Kristinkass <144677531+Kristinkass@users.noreply.github.com>
2025-03-23 12:12:16 +0300

    Add files via upload

---
+++ site/config1.py
@@ -0,0 +1,6 @@
+db_config = {
+    'host': '127.0.0.1',  # Адрес сервера БД
+    'user': 'root',       # Имя пользователя БД
+    'password': 'Cstud161620', # Пароль
+    'database': 'horoshev36_prj' # Имя вашей БД
+}
